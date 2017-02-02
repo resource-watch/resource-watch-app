e54a08767d1c498a8a627e6389bbf17ccf03344e
David Inga <david.inga@vizzuality.com>
2017-02-02 17:35:53 +0100

    added basic auth

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,7 +3,17 @@ const express = require('express');
 
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
+function auth(username, password) {
+  const user = basicAuth(req);
+  if (!user || user.name !== username || user.pass !== password) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  }
+  return next();
+}
+
 module.exports = (app) => {
+  app.use(auth(process.env.USERNAME, process.env.PASSWORD));
   app.use(express.static(path.join(process.cwd(), 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(indexPath);
