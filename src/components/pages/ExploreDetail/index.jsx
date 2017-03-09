e87f59229c0deeeb23b58991873a2e51580b52ff
Pablo Pareja <pabloparejatobes@gmail.com>
2017-03-09 15:24:30 +0100

    Set widget/layer title in the header and apply some styling to it

--- src/components/pages/ExploreDetail/index.jsx
+++ src/components/pages/ExploreDetail/index.jsx
@@ -4,12 +4,11 @@ import React from 'react';
 import Title from 'components/ui/Title';
 import Breadcrumbs from 'components/ui/Breadcrumbs';
 
-
 // Styles
 import './style.scss';
 
 const breadcrumbs = [
-  {name: 'Home', url: '/'}
+  { name: 'Home', url: '/' }
 ];
 
 class ExploreDetail extends React.Component {
@@ -23,16 +22,40 @@ class ExploreDetail extends React.Component {
   }
 
   componentWillMount() {
+    // temporal fix to clear the fields
+
     this.props.getDataset(this.props.params.id);
   }
 
+  componentWillReceiveProps(nextProps){
+
+  }
+
   render() {
-    console.info(this.props.exploreDetail);
+    const attributes = this.props.exploreDetail.dataset.detail.attributes;
+
+    let titleText = '';
+
+    if (attributes) {
+      if (attributes.widget[0]) {
+        const widget = attributes.widget[0].attributes;
+        console.info('widget', widget);
+        titleText = widget.name;
+      }
+
+      if (attributes.layer[0]) {
+        const layer = attributes.layer[0].attributes;
+        console.info('layer', layer);
+        titleText = layer.name;
+      }
+    }
 
     return (
-      <div className="c-page">
-        <Breadcrumbs items={breadcrumbs} />
-        <Title className="-p`rimary -huge" >Hey!</Title>
+      <div className="c-page c-page-explore-detail">
+        <div className="header">
+          <Breadcrumbs items={breadcrumbs} />
+          <Title className="-primary -huge" >{titleText}</Title>
+        </div>
       </div>
     );
   }
