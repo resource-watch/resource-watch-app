83cf3f9d42e7c0093fee98f2be2f81cb6b63669e
bitTal <albafjez@gmail.com>
2017-03-15 12:28:00 +0100

    Get involved page structure

--- src/components/pages/GetInvolved.jsx
+++ src/components/pages/GetInvolved.jsx
@@ -8,7 +8,7 @@ function GetInvolved() {
   const intro = ['Lorem ipsum sit amet casius sem', 'lacinia quam venenatis vestibulum'];
 
   return (
-    <div className="p-about">
+    <div className="p-get-involved">
       <div className="c-page about">
         <Intro title={title} intro={intro} />
         <section className="l-section">
@@ -25,7 +25,7 @@ function GetInvolved() {
           <div className="column small-12">
             <Banner className="partners">
               <h3 className="c-text -header-normal -thin">See yourself as part <br/>of this team?</h3>
-              <button className="c-btn -primary -filled">
+              <button className="c-btn -primary">
                 Get in touch
               </button>
             </Banner>
