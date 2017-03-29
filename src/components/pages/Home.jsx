6bc1bc51f4c4ea996f895c27135c619a6bd833f7
bitTal <albafjez@gmail.com>
2017-03-29 12:03:47 +0200

    Insights section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 import Banner from 'components/common/Banner';
 import CardStatic from 'components/common/CardStatic';
+import Rating from 'components/common/Rating';
 
-const cards = [
+const exploreCards = [
   {
     tag: 'Explore datasets',
     title: 'Dive into the data',
@@ -45,13 +46,40 @@ const cards = [
   }
 ];
 
+const insightsCards = [
+  {
+    tag: 'INSIGHT OF THE WEEK',
+    title: 'A factory is being built in your neighborhood. Can you do anything about it?',
+    slug: 'interactive-edi',
+    source: { name: 'World Resources Institute', path: '#', img: 'https://vizzuality.github.io/WRW-Prototype/img/avatar-wri.png'},
+    ranking: 3531,
+    background: 'url(/images/backgrounds/jellyfish.jpg)'
+  },
+  {
+    tag: 'Oct 5, 2015',
+    title: 'The Water Guardians of the Andes',
+    slug: 'slideshow-peru',
+    source: { name: 'World Resources Institute', path: '#', img: 'https://vizzuality.github.io/WRW-Prototype/img/avatar-wri.png'},
+    ranking: 4345,
+    background: 'url(/images/backgrounds/jellyfish.jpg)'
+  },
+  {
+    tag: 'Oct 5, 2015',
+    title: 'Farms to feel squeeze as competition for water increases',
+    slug: 'interactive-map',
+    source: { name: 'World Resources Institute', path: '#', img: 'https://vizzuality.github.io/WRW-Prototype/img/avatar-wri.png'},
+    ranking: 4345,
+    background: 'url(/images/backgrounds/jellyfish.jpg)'
+  }
+];
+
 function Home() {
-  const cardsStatic = cards.map((c, i) => 
+  const exploreCardsStatic = exploreCards.map((c, i) => 
     <div key={i} className="column small-12 medium-4">
       <CardStatic className='-light' background={c.background}>
         <div>
           <h5 className="tag c-text -small -bold -uppercase">{c.tag}</h5>
-          <h3 className="title c-text -extra-big -bold">{c.title}</h3>
+          <h1 className="title c-text -extra-big -bold">{c.title}</h1>
           <p className="c-text -big">{c.intro}</p>
         </div>
         <div className="buttons">
@@ -65,30 +93,83 @@ function Home() {
     </div>
   );
 
+  const insightsCardsStatic = insightsCards.map((c, i) =>
+    <CardStatic key={i} className='-light' background={c.background}>
+      <div>
+        <h5 className="tag c-text -small -bold -uppercase">{c.tag}</h5>
+        <h1 className="title c-text -extra-big -bold"><a href={`/insights/${c.slug}`}>{c.title}</a></h1>
+      </div>
+      <div className="footer">
+        <div className="source">
+          <img src={c.source.img || ''} />
+          <div className="source-name">
+            by <a href={c.source.path}>{c.source.name}</a>
+          </div>
+        </div>
+        <Rating rating={c.ranking}/>
+      </div>
+    </CardStatic>
+  );
+
   return (
     <div className="p-home">
       <div className="c-page">
         <Banner className="intro">
-          <h3 className="title c-text -header-huge -thin">Quick and easy access<br/>to a world of resource data</h3>
+          <h1 className="title c-text -header-huge -thin">Quick and easy access<br/>to a world of resource data</h1>
           <p className="c-text -huge -thin">Explore the latest data, make insights, and help build a more sustainable planet</p>
         </Banner>
 
-        <section className="l-section -bg-grey">
+        <section className="l-section insights">
           <div className="l-container">
             <header className="row">
               <div className="column small-12 medium-8">
-                <h1 className="c-text -header-big -primary -thin">Explore the data</h1>
+                <h1 className="title c-text -header-big -primary -thin">Discover Insights</h1>
               </div>
             </header>
 
             <div className="row">
               <article className="column small-12 medium-5">
-                <p className="c-text -extra-big">Explore, create visualizations, receive updates and contribute with your data.</p>
+                <p className="intro c-text -extra-big">Read the latest  analysis from our community or submit your own original story.</p>
               </article>
             </div>
 
             <div className="row">
-              {cardsStatic}
+              <div className="column small-12 medium-8">
+                {insightsCardsStatic[0]}
+              </div>
+
+              <div className="column small-12 medium-4">
+                <div className="dual">
+                  {insightsCardsStatic[1]}
+                  {insightsCardsStatic[2]}
+                </div>
+              </div>
+            </div>
+            <div className="row buttons">
+              <div className="column small-12 medium-4 medium-offset-4">
+                <a href="/insights" className="c-btn -filled -primary">More insights</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        
+
+        <section className="l-section -bg-grey explore">
+          <div className="l-container">
+            <header className="row">
+              <div className="column small-12 medium-8">
+                <h1 className="title c-text -header-big -primary -thin">Explore the data</h1>
+              </div>
+            </header>
+
+            <div className="row">
+              <article className="column small-12 medium-5">
+                <p className="intro c-text -extra-big">Explore, create visualizations, receive updates and contribute with your data.</p>
+              </article>
+            </div>
+
+            <div className="row">
+              {exploreCardsStatic}
             </div>
           </div>
         </section>
@@ -96,7 +177,7 @@ function Home() {
         <Banner className="get-involved">
           <div className="row">
             <div className="column small-12 medium-6">
-              <h3 className="title c-text -header-huge -thin">Get Involved</h3>
+              <h1 className="title c-text -header-huge -thin">Get Involved</h1>
               <p className="c-text -big">We've brought together the best datasets related to natural resources, so you can find new insights, influence decisions and change the world. There's a world of opportunity to take this futher; here’s some ideas to get you started...</p>
             </div>
           </div>
